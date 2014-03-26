011890ee51b1d6abd0b306c725b0b043988b56bb
Nate Finch <nate.finch@gmail.com>
2014-03-26 14:29:19 -0400

    add code

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+)
+
+func main() {
+
+	cmd := exec.Command("go", "test", "./...")
+	p, err := cmd.StdoutPipe()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error getting StdoutPipe from command: %v", err)
+		os.Exit(1)
+	}
+	if err := cmd.Start(); err != nil {
+		fmt.Fprintf(os.Stderr, "Error running command: %v", err)
+		os.Exit(1)
+	}
+	scanner := bufio.NewScanner(p)
+	for scanner.Scan() {
+		if !strings.HasPrefix(string(scanner.Bytes()), "[LOG]") {
+			fmt.Println(string(scanner.Bytes()))
+		}
+	}
+	if scanner.Err() != nil {
+		fmt.Fprintf(os.Stderr, "Error reading output of go test: %v", scanner.Err())
+		os.Exit(1)
+	}
+}
